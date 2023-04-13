b556df31e844a584c22d3d86c356aef4291a6c54
MaxChevalier <maxime.chevalier@ynov.com>
2023-04-13 21:41:09 +0200

    Leave area

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -12,12 +12,13 @@ public class Player : MonoBehaviour
     public Transform lightTransform = null;
     public float walkSpeed = 1f;
     public float sprintSpeed = 2f;
+    public bool canMove = true;
 
     private float speed = 0f;
-    private Vector2 Movement = Vector2.zero;
+    public Vector2 Movement = Vector2.zero;
 
     public bool isOnGamepad = false;
-    private Animator animator;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,34 +39,43 @@ public class Player : MonoBehaviour
 
     void OnMove(InputValue value)
     {
-        Movement = value.Get<Vector2>();
-        if (Movement.y > ((Mathf.Sqrt(2))/2) ) {
-            animator.SetInteger("mouvement", 1);
-            transform.GetChild(1).transform.position = new Vector3(0.50f, 0.09f, 0f)+transform.position;       
+        if (canMove)
+        {
+            Movement = value.Get<Vector2>();
+            if (Movement.y > ((Mathf.Sqrt(2)) / 2))
+            {
+                animator.SetInteger("mouvement", 1);
+                transform.GetChild(1).transform.position = new Vector3(0.50f, 0.09f, 0f) + transform.position;
             }
-        else if (Movement.y < (-(Mathf.Sqrt(2))/2) ){ 
-            animator.SetInteger("mouvement", 2);
-            transform.GetChild(1).transform.position = new Vector3(0.50f, 0.09f, 0f)+transform.position;
+            else if (Movement.y < (-(Mathf.Sqrt(2)) / 2))
+            {
+                animator.SetInteger("mouvement", 2);
+                transform.GetChild(1).transform.position = new Vector3(0.50f, 0.09f, 0f) + transform.position;
+            }
+            else if (Movement.x >= ((Mathf.Sqrt(2)) / 2))
+            {
+                animator.SetInteger("mouvement", 4);
+                transform.GetChild(1).transform.position = new Vector3(0.88f, 0.11f, 0f) + transform.position;
+            }
+            else if (Movement.x <= (-(Mathf.Sqrt(2)) / 2))
+            {
+                animator.SetInteger("mouvement", 3);
+                transform.GetChild(1).transform.position = new Vector3(-0.88f, 0.11f, 0f) + transform.position;
+            }
+            else animator.SetInteger("mouvement", 0);
         }
-        else if (Movement.x >= ((Mathf.Sqrt(2))/2) ){
-             animator.SetInteger("mouvement", 4);
-             transform.GetChild(1).transform.position = new Vector3(0.88f, 0.11f, 0f)+transform.position;
-        }
-        else if (Movement.x <= (-(Mathf.Sqrt(2))/2) ){
-             animator.SetInteger("mouvement", 3);
-             transform.GetChild(1).transform.position = new Vector3(-0.88f, 0.11f, 0f)+transform.position;
-        }
-        else animator.SetInteger("mouvement", 0);
-
     }
 
     void UpdateLight()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Mouse.current.position.x.ReadValue(), Mouse.current.position.y.ReadValue()));
-        Vector2 direction = new Vector2(mousePos.x - selfTransform.position.x, mousePos.y - selfTransform.position.y);
-        direction.Normalize();
-        float angle = Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg;
-        lightTransform.rotation = Quaternion.Euler(0, 0, angle);
+        if (canMove)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Mouse.current.position.x.ReadValue(), Mouse.current.position.y.ReadValue()));
+            Vector2 direction = new Vector2(mousePos.x - selfTransform.position.x, mousePos.y - selfTransform.position.y);
+            direction.Normalize();
+            float angle = Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg;
+            lightTransform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 
     void OnSprintStart()
